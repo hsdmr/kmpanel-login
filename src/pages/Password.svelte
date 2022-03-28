932f49190343dce7e609875cd2b4c39816e6c435
hsdmr <hsdmrsoft@gmail.com>
2022-03-28 03:39:48 +0300

    change bind to body

--- src/pages/Password.svelte
+++ src/pages/Password.svelte
@@ -4,42 +4,44 @@
   import Lang from "src/components/Lang.svelte";
 </script>
 
-<div class="login-box">
-  <div class="card card-outline card-success">
-    <div class="card-header text-center">
-      <Lang />
-      <a href="/" class="h1"><b>KM</b>PANEL</a>
-    </div>
-    <div class="card-body">
-      <p class="login-box-msg">
-        {$__("password.message")}
-      </p>
-      <form action="recover-password.html" method="post">
-        <div class="input-group mb-3">
-          <input
-            type="email"
-            class="form-control"
-            placeholder={$__("title.email")}
-          />
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-envelope" />
+<div class="login-page">
+  <div class="login-box">
+    <div class="card card-outline card-success">
+      <div class="card-header text-center">
+        <Lang />
+        <a href="/" class="h1"><b>KM</b>PANEL</a>
+      </div>
+      <div class="card-body">
+        <p class="login-box-msg">
+          {$__("password.message")}
+        </p>
+        <form action="recover-password.html" method="post">
+          <div class="input-group mb-3">
+            <input
+              type="email"
+              class="form-control"
+              placeholder={$__("title.email")}
+            />
+            <div class="input-group-append">
+              <div class="input-group-text">
+                <span class="fas fa-envelope" />
+              </div>
             </div>
           </div>
-        </div>
-        <div class="row">
-          <div class="col-12">
-            <button type="submit" class="btn btn-success btn-block"
-              >{$__("password.requestNew")}</button
-            >
+          <div class="row">
+            <div class="col-12">
+              <button type="submit" class="btn btn-success btn-block"
+                >{$__("password.requestNew")}</button
+              >
+            </div>
+            <!-- /.col -->
           </div>
-          <!-- /.col -->
-        </div>
-      </form>
-      <p class="mt-3 mb-1">
-        <Link to="/login">{$__("password.login")}</Link>
-      </p>
+        </form>
+        <p class="mt-3 mb-1">
+          <Link to="/login">{$__("password.login")}</Link>
+        </p>
+      </div>
+      <!-- /.login-card-body -->
     </div>
-    <!-- /.login-card-body -->
   </div>
 </div>
